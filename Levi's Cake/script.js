bc6ccea500a82a6665ed7f3849653c0d475df598
yuki6464 <106538691+yuki6464@users.noreply.github.com>
2022-11-29 11:05:53 +0200

    Add files via upload

--- Levi's Cake/script.js
+++ Levi's Cake/script.js
@@ -1,4 +1,21 @@
-let search = document.querySelector('.search');
-document.querySelector('#search').onclick=() =>{
-    search.classList.toggle('active');
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
 }
